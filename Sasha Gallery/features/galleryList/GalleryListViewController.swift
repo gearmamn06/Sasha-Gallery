45a637c8b96ff192dea3fddb8bf75a56a03f8d46
박현수 <sudo@amanda.co.kr>
2019-07-03 01:39:40 +0900

    GalleryListView 필요 파일 추가

---
+++ Sasha Gallery/features/galleryList/GalleryListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GalleryListViewController.swift
+//  Sasha Gallery
+//
+//  Created by ParkHyunsoo on 2019/07/03.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
---
+++ Sasha Gallery/features/galleryList/GalleryListViewModelType.swift
@@ -0,0 +1,9 @@
+//
+//  GalleryListViewModelType.swift
+//  Sasha Gallery
+//
+//  Created by ParkHyunsoo on 2019/07/03.
+//  Copyright © 2019 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
